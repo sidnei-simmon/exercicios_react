b713162a6ec77f2dc4ec349c892a5fbe5e09767c
= <=>
2017-10-08 22:04:19 -0300

    #Listagem 66 - Exportar multiplos componentes

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>
-}
+export default props => (
+    <h1>Primeiro Componente</h1>
+)
+
+export const Segundo = props => (
+    <h1>Segundo Componente</h1>
+)
+
+//export {Primeiro, Segundo}
