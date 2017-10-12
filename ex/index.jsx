8b3a6519fe03b09de23d7843aeb37728d7f42abf
= <=>
2017-10-12 11:31:09 -0300

    Listagem 80 - Componente Classe

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
-import Member from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
     <div>
-    <Family lastName='Silva'>
-        <Member name='Guilherme'/>
-        <Member name='Sidnei'/>
-    </Family>
+    <ClassComponent value='Hora do café'/>
 </div>, document.getElementById('app'))
